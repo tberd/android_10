87c6843eae3723a6861c2b6559b1373ec83c173c
Joshua Primero <jprimero155@gmail.com>
2020-02-09 12:43:51 -0800

    Space suckz

--- los.sh
+++ los.sh
@@ -10,7 +10,7 @@ git cherry-pick 4a9c4129 # libstagefright_wfd: libmediaplayer2: compilation fixe
 git cherry-pick f168b4e8 # stagefright: Fix SurfaceMediaSource getting handle from wrong position
 git cherry-pick 6f7f4e34 # stagefright: Fix buffer handle retrieval in signalBufferReturned
 git cherry-pick 87dcc138 # libstagefright_wfd: video encoder does not actually release MediaBuffer
-git cherry-pick  e883e76e # Revert "Move unused classes out of stagefright foundataion"
+git cherry-pick e883e76e # Revert "Move unused classes out of stagefright foundataion"
 git cherry-pick eb16bec7 # Remove libmediaextractor dependency from libstagefright_wfd and Surface
 git cherry-pick d00e3f1c # audioflinger: Fix audio for WifiDisplay
 cd .. && cd .. 
