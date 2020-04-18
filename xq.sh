7687be21157dc97b875c3fc3b4f57e431d378661
Joshua Primero <jprimero155@gmail.com>
2020-04-17 20:44:35 -0700

    Update xq.sh

--- xq.sh
+++ xq.sh
@@ -5,3 +5,10 @@ git -C packages/providers/TelephonyProvider fetch https://github.com/Claymore129
 
 # home wake
 git -C frameworks/base fetch https://github.com/Staydirtyboi/frameworks_base xq && git -C frameworks/base cherry-pick a64c28c261463a497a8c632d30ce8378ed91c634 
+
+# lockscreen battery charge info fix
+git -C frameworks/base fetch https://github.com/Staydirtyboi/frameworks_base xq && git -C frameworks/base cherry-pick a581994a4fb6a7a2564ce4f61f2a61f718dd1e11 
+git -C frameworks/base fetch https://github.com/Staydirtyboi/frameworks_base xq && git -C frameworks/base cherry-pick aea0c6b914fb99171d5c94ddf3547c195fac4dfe 
+
+# Fix compile
+git -C frameworks/av revert --no-edit cf5b100a734071d1698564911254783f1eae9ea1
