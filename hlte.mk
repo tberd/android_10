2dbfbacf0d9f15ba33d87f7c3dd0b9519d097b80
Joshua Primero <jprimero155@gmail.com>
2020-04-21 02:05:32 -0700

    hlte-common: crdroid: Build OP Recorder
    Signed-off-by: Joshua Primero <jprimero1555@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
+# OnePlus Recorder
+PRODUCT_PACKAGES += \
+    OPScreenRecorder
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
