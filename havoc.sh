6761163eb4522a38cff4a7d05b4a6f09106a61df
Joshua Primero <jprimero155@gmail.com>
2020-04-22 01:56:12 -0700

    Update havoc.sh

--- havoc.sh
+++ havoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ril sprint fix
-git -C packages/providers/TelephonyProvider fetch https://github.com/Claymore1297/packages_providers_TelephonyProvider q10.0 && git -C packages/providers/TelephonyProvider cherry-pick a1e2b2e355f47aadea78599a746081ebaebc73bd
+git -C packages/providers/TelephonyProvider fetch https://github.com/Jprimero15/packages_providers_TelephonyProvider q10.0 && git -C packages/providers/TelephonyProvider cherry-pick a1e2b2e355f47aadea78599a746081ebaebc73bd
 
 # home wake
 git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 99e2e8deed64c159736142bcd541c68d1d9b6966
