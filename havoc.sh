4bf4bbe3e30a847b895960613d74d68a85a19461
Joshua Primero <jprimero155@gmail.com>
2020-04-23 11:28:28 -0700

    Update havoc.sh

--- havoc.sh
+++ havoc.sh
@@ -13,6 +13,9 @@ git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_
 # lockscreen battery charge info fix
 git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 2898028a61ca52defb5efd2e8068d3d6ba211563
 
+# shutup pwr
+git -C vendor/qcom/opensource/power fetch https://github.com/Jprimero15/android_10 power && git -C vendor/qcom/opensource/power cherry-pick 426144bad449ada7c3a98b15be4e5403c3693d2b
+
 # enabl3 screen record boi
 git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick cb60979075bc91af518284670dc4550602748472 
 
