06e52cdd27e73053a27c2e227a67a0944638c461
Joshua Primero <jprimero155@gmail.com>
2020-04-24 04:46:34 -0700

    Update havoc.sh

--- havoc.sh
+++ havoc.sh
@@ -19,17 +19,6 @@ git -C vendor/qcom/opensource/power fetch https://github.com/Jprimero15/android_
 # enabl3 screen record boi
 git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick cb60979075bc91af518284670dc4550602748472 
 
-# fw/av picks
-cd frameworks/av
-git fetch https://github.com/crdroidandroid/android_frameworks_av 10.0-20200203
-git cherry-pick 5147925b # ld.config: add libashmemd_client.so to platform shared libs
-git cherry-pick b8772262 # Revert "Removed unused class and its test"
-git cherry-pick bf64535c # Revert "stagefright: remove Miracast sender code"
-git cherry-pick 4a9c4129 # libstagefright_wfd: libmediaplayer2: compilation fixes
-git cherry-pick f168b4e8 # stagefright: Fix SurfaceMediaSource getting handle from wrong position
-git cherry-pick 6f7f4e34 # stagefright: Fix buffer handle retrieval in signalBufferReturned
-git cherry-pick 87dcc138 # libstagefright_wfd: video encoder does not actually release MediaBuffer
-git cherry-pick e883e76e # Revert "Move unused classes out of stagefright foundataion"
-git cherry-pick eb16bec7 # Remove libmediaextractor dependency from libstagefright_wfd and Surface
-git cherry-pick d00e3f1c # audioflinger: Fix audio for WifiDisplay
-cd .. && cd .. 
+# fw/av picks wfd
+git -C frameworks/av fetch https://github.com/Jprimero15/android_10/tree/fw_av&& git -C frameworks/av cherry-pick 55e086441abc08aeee583cbbe3ac9c4aeb874cd5
+
