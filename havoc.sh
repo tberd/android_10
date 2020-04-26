cbaaf1fbc255b9b2fec491f1866dac6d612a4720
Joshua Primero <jprimero155@gmail.com>
2020-04-25 22:50:20 -0700

    Update havoc.sh

--- havoc.sh
+++ havoc.sh
@@ -4,7 +4,7 @@
 git -C packages/providers/TelephonyProvider fetch https://github.com/Jprimero15/packages_providers_TelephonyProvider q10.0 && git -C packages/providers/TelephonyProvider cherry-pick a1e2b2e355f47aadea78599a746081ebaebc73bd
 
 # replace doze package
-git -C packages/apps/ConfigCenter fetch https://github.com/Staydirtyboi/android_packages_apps_ConfigCenter ten && git -C packages/apps/ConfigCenter cherry-pick c67d6bc20f5a7736e6663df2ce9e6b56140b3d34
+#git -C packages/apps/ConfigCenter fetch https://github.com/Staydirtyboi/android_packages_apps_ConfigCenter ten && git -C packages/apps/ConfigCenter cherry-pick c67d6bc20f5a7736e6663df2ce9e6b56140b3d34
 
 # home wake
 git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 99e2e8deed64c159736142bcd541c68d1d9b6966
