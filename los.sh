ffb17f5c97f949a6050d03924bc6400cd897d16a
Joshua Primero <jprimero155@gmail.com>
2020-04-28 11:37:02 -0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -3,6 +3,9 @@
 # ril sprint fix
 git -C packages/providers/TelephonyProvider fetch https://github.com/Claymore1297/packages_providers_TelephonyProvider q10.0 && git -C packages/providers/TelephonyProvider cherry-pick a1e2b2e355f47aadea78599a746081ebaebc73bd
 
+# lockscreen batt fix
+git -C frameworks/base fetch https://github.com/Staydirtyboi/frameworks_base xq && git -C frameworks/base cherry-pick f8775fdbf99d2fb719bda6d7803fd1122cbcdb17
+
 Shutup pwr
 git -C vendor/qcom/opensource/power fetch https://github.com/Jprimero15/android_10 power && git -C vendor/qcom/opensource/power cherry-pick 426144bad449ada7c3a98b15be4e5403c3693d2b
 
