10a371aba347e8ffee24610d2cbec4a88f53de07
Joshua Primero <jprimero155@gmail.com>
2020-05-08 01:40:22 -0700

    Update havoc.sh

--- havoc.sh
+++ havoc.sh
@@ -7,8 +7,8 @@ git -C packages/providers/TelephonyProvider fetch https://github.com/Jprimero15/
 #git -C packages/apps/ConfigCenter fetch https://github.com/Staydirtyboi/android_packages_apps_ConfigCenter ten && git -C packages/apps/ConfigCenter cherry-pick c67d6bc20f5a7736e6663df2ce9e6b56140b3d34
 
 # home wake
-git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 99e2e8deed64c159736142bcd541c68d1d9b6966
-git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 8e12af5bcbeeef9762b67ae1c390611b3f718919
+git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 911409de60a7d7cdd3cac7d4268462da77cb32c3
+git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 449804e18c7effe333d5dbef03c5ab5bb32f0521
 
 # lockscreen battery charge info fix
 git -C frameworks/base fetch https://github.com/Staydirtyboi/android_frameworks_base-1 ten && git -C frameworks/base cherry-pick 2898028a61ca52defb5efd2e8068d3d6ba211563
